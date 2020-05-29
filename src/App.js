8fceca2582e0701e0d554de188732404b1df4174
Matilda-bit <polinaovras@gmail.com>
2020-05-29 23:04:20 +0300

    using route & switch, nav to Auth screen

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import Translate from './components/Translate/Translate';
-import './App.css';
+import Layout from "./components/Layout/Layout";
+import Translate from "./components/Translate/Translate";
+import Auth from "./containers/Auth/Auth";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Layout>
-            <h1>Welcome!</h1>
-            <Translate />
-          </Layout>
+        <Layout>
+          <Switch>
+            <Route path="/auth" component={Auth} />
+          </Switch>
+        </Layout>
       </div>
     );
   }
 }
 
+// <h1>Welcome!</h1>
+// <Translate />
 export default App;
