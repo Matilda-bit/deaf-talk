88d8e404cc858848d48476161ea7c6f8725dceac
Matilda-bit <polinaovras@gmail.com>
2020-05-30 20:01:27 +0300

    add auth

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import './Toolbar.css'
-import Logo from '../../Logo/Logo';
+import React from "react";
+
+import "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+//import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+
 const toolbar = (props) => (
-    <header className="Toolbar">
-        <div className="ToolbarText">Deaf Talk</div>
-        <Logo />
-        <nav>
-            ...
-        </nav>
+  <header className="Toolbar">
+    <div className="ToolbarText">Deaf Talk</div>
+    <div>
+      <Logo className="Logo" />
+    </div>
 
-    </header>
+    <nav className="DesktopOnly">
+      <NavigationItems className="ToolbarText" />
+    </nav>
+  </header>
 );
+// <div className="Logo">
+// <Logo />
+// </div>
 
+// <DrawerToggle clicked={props.drawerToggleClicked} />
+
+// <nav className="DesktopOnly">
+//   <NavigationItems className="ToolbarText" />
+// </nav>
 export default toolbar;
