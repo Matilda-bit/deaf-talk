281aacb8962daefa8cd673ead5aebdd1a68a6499
Matilda-bit <polinaovras@gmail.com>
2020-08-03 18:15:58 +0300

    logout after one hour

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,21 @@ export const authFail = (error) => {
     error: error,
   };
 };
-//react guide 326/
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
+
 export const auth = (email, password, isSignup) => {
   return (dispatch) => {
     dispatch(authStart());
@@ -42,6 +56,7 @@ export const auth = (email, password, isSignup) => {
       .then((response) => {
         console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch((err) => {
         dispatch(authFail(err.response.data.error));
