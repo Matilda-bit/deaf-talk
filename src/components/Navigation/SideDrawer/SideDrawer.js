5f0987d07e07cabaee744e73a943e12215cbc797
Matilda-bit <polinaovras@gmail.com>
2020-08-08 15:43:30 +0300

    configure

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import React from "react";
 //import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import * as classes from "./SideDrawer.css";
-import "./SideDrawer.css";
+//import "./SideDrawer.css";
 
 import Backdrop from "../../UI/Backdrop/Backdrop";
 import Aux from "../../../hoc/Aux1/Aux";
@@ -18,7 +18,7 @@ const sideDrawer = (props) => {
       <Backdrop show={props.open} clicked={props.closed} />
       <div className={attachedClasses.join(" ")}>
         <nav>
-          <NavigationItems />
+          <NavigationItems isAuthenticated={props.isAuth} />
         </nav>
       </div>
     </Aux>
