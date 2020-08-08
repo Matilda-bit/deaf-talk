e47781f1818d4464ffb93575fc8356ae403f278a
Matilda-bit <polinaovras@gmail.com>
2020-08-08 20:06:03 +0300

    add img & button for Name lesson

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 import classes from "./HomePage.css";
 import Icon from "../UI/Icon/Icon";
 import Button from "../../components/UI/Button/Button";
+import ProgressBar from "../UI/ProgressBar/ProgressBar";
 
+const testData = [{ bgcolor: "#00695c", completed: 30 }];
 class HomePage extends Component {
   render() {
     return (
       <div className={classes.Container}>
-        <h1>Your Name</h1>
+        <h1>Hello Friend!</h1>
+        <div>
+          {testData.map((item, idx) => (
+            <ProgressBar
+              key={idx}
+              bgcolor={item.bgcolor}
+              completed={item.completed}
+            />
+          ))}
+        </div>
+
         <Button
           clicked={() => {
             this.props.history.push("/PickLesson");
